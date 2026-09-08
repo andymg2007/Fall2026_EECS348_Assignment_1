d7aa9815e547d19d4f143e5e1cf6ce52c56379a9
Garcia <a455g434@engr1005-01.home.ku.edu>
2026-09-08 13:35:45 -0500

    First commit of Claude's code

---
+++ 3205430_Garcia_Assignment_1/secret_number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void){
+    int secret = 7;
+    int guess;
+    int attempts = 3;
+    int won = 0;
+
+    printf("Guess a number between 1 and 10.\n");
+
+    for (int i = 1; i <= attempts; i++) {
+        printf("Attempt %d/%d. Enter your guess: ", i, attempts);
+        scanf("%d", &guess);
+
+        if (guess == secret) {
+            printf("Correct! You win!\n");
+            won =1;
+            break;
+        } else if (guess < secret) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Too high! Try again.\n");
+        }
+    }
+
+    if (!won) {
+        printf("You lose! The number was %d.\n", secret);
+    }
+
+    return 0;
+}
