d18e0a457a2fc6c188934c56b3f7eb0562f3fb11
Garcia <a455g434@engr1005-01.home.ku.edu>
2026-09-08 13:37:18 -0500

    Added some comments

--- 3205430_Garcia_Assignment_1/secret_number.c
+++ 3205430_Garcia_Assignment_1/secret_number.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>   // include the standard input output library
 
-int main(void){
-    int secret = 7;
-    int guess;
-    int attempts = 3;
-    int won = 0;
+int main(void){  // initialize main
+    int secret = 7;  // create variable secret as an int equal to 7
+    int guess;   // create variable guess as an int to be used for user input
+    int attempts = 3; // create variable attempts as an int equal to 3
+    int won = 0;  // create variable won as an int equal to 0
 
-    printf("Guess a number between 1 and 10.\n");
+    printf("Guess a number between 1 and 10.\n");   // prints prompt to the user to get their guess
 
-    for (int i = 1; i <= attempts; i++) {
-        printf("Attempt %d/%d. Enter your guess: ", i, attempts);
-        scanf("%d", &guess);
+    for (int i = 1; i <= attempts; i++) {    // initialize for loop to run code below as many times as attempts is equal to
+        printf("Attempt %d/%d. Enter your guess: ", i, attempts);    // prints which guess out of total attempts it is and asks user for another guess
+        scanf("%d", &guess);  
 
         if (guess == secret) {
             printf("Correct! You win!\n");
